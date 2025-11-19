619577679c4765aa3e1a747ac50f8e99a352cfde
Bertalan Lovas <lovas.bertalan97@gmail.com>
2025-11-20 00:14:05 +0100

    Init commit

---
+++ src/main/java/com/urlshortener/UrlShortener/controllers/UrlController.java
@@ -0,0 +1,15 @@
+package com.urlshortener.UrlShortener.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+public class UrlController {
+
+    @GetMapping("/google")
+    public RedirectView goToGoogle(){
+        return new RedirectView("https://google.com");
+    }
+}
